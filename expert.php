ffc392424f60ed7c25bf2057cef72f5e0aba4571
xandervdh <xander-v.d.h@hotmail.com>
2020-10-02 10:15:33 +0200

    fixed excersize 2. array starts at 0 not 1git rm -r --cache .idea/git rm -r --cache .idea/

--- expert.php
+++ expert.php
@@ -7,12 +7,18 @@ error_reporting(E_ALL);
 
 // === Exercise 1 ===
 // $x isn't defined so i put it in the function arguments
-
+echo "Exercise 1 starts here:";
 
 function new_exercise($x) {
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
     echo $block;
 }
-new_exercise(1);
 
+new_exercise(2);
+// === Exercise 2 ===
+//you try to print monday but an array starts at 0 so just changed 1 to 0
 
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
