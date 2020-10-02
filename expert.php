520d3f4a7663929eb6b6004c5713914a3aba67c8
xandervdh <xander-v.d.h@hotmail.com>
2020-10-02 14:16:24 +0200

    fixed excersize 10 by making the count of the array a variable

--- expert.php
+++ expert.php
@@ -141,3 +141,18 @@ echo isLinkValid('https://google.com');
 echo isLinkValid('http://google.com');
 //VALID link
 echo isLinkValid('http://google.com/test.txt');
+
+new_exercise(10);
+
+//Filter the array $areTheseFruits to only contain valid fruits
+//do not change the arrays itself
+$areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
+$validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
+//from here on you can change the code
+$count = count($areTheseFruits);
+for($i=0; $i < $count; $i++) {
+    if(!in_array($areTheseFruits[$i], $validFruits)) {
+        unset($areTheseFruits[$i]);
+    }
+}
+var_dump($areTheseFruits);//do not change this
